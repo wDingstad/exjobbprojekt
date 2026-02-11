ce00694b104a0f711a7f7bbe7bd3ab6651cc9130
William <dingstad.william@gmail.com>
2026-02-11 11:19:15 +0100

    added crud endpoints for user

--- backend/src/main/java/com/example/Backend/controller/UserController.java
+++ backend/src/main/java/com/example/Backend/controller/UserController.java
@@ -48,6 +48,30 @@ public class UserController {
         return ResponseEntity.ok(cases);
     }
 
+    @PostMapping("/users/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Integer id, @RequestBody User updateUser){
+        return userRepository.findById(id)
+                .map(user -> {
+                    user.setFirst_name(updateUser.getFirst_name());
+                    user.setLast_name(updateUser.getLast_name());
+                    user.setEmail(updateUser.getEmail());
+                    user.setPhone_number(updateUser.getEmail());
+                    User saved = userRepository.save(user);
+                    return ResponseEntity.ok(saved);
+                })
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/users/{id}")
+    public ResponseEntity<?> deleteUser(@PathVariable Integer id){
+        return userRepository.findById(id)
+                .map(user -> {
+                    userRepository.delete(user);
+                    return ResponseEntity.ok().build();
+                })
+                .orElse(ResponseEntity.notFound().build());
+    }
+
 
 
 
